e2beccdd334fac0dbb944bdb6db0565ab646b6de
kobayashi <hiroyuki.kobayashi@oit.ac.jp>
2025-04-28 14:57:04 +0900

    edit(client): 0428 weather

--- wsdclient/lib/main.dart
+++ wsdclient/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -33,8 +34,16 @@ class _MyHomePageState extends State<MyHomePage> {
   final _apiKeyWeather = "31c33ba0fa0fbe84f0af7ae3778818ce";
   int _counter = 0;
   int _cityIndex = 0;
+  String _weatherText = "---";
+  String? _iconId;
 
-  final _cities = [("大阪", "osaka"), ("名古屋", "nagoya"), ("東京", "tokyo")];
+  final _cities = [
+    ("大阪", "osaka"),
+    ("名古屋", "nagoya"),
+    ("東京", "tokyo"),
+    ("フロリダ", "florida"),
+    ("ロンドン", "london"),
+  ];
 
   void _incrementCounter() {
     setState(() {
@@ -56,22 +65,41 @@ class _MyHomePageState extends State<MyHomePage> {
             Expanded(
                 flex: 1,
                 child: Row(children: [
-                  Expanded(flex: 3, child: Text(_cities[_cityIndex].$1)),
-                  Expanded(flex: 1, child: Text("天気")),
+                  Expanded(
+                      flex: 3,
+                      child: DropdownMenu<int>(
+                        dropdownMenuEntries: [
+                          for (var i = 0; i < _cities.length; i++)
+                            DropdownMenuEntry<int>(
+                                value: i, label: _cities[i].$1),
+                        ],
+                        onSelected: (int? newVal) async {
+                          setState(() {
+                            if (newVal != null) _cityIndex = newVal;
+                          });
+                          final resp = await http.get(Uri.parse(
+                              "https://api.openweathermap.org/data/2.5/weather?q=${_cities[_cityIndex].$2}&appid=$_apiKeyWeather"));
+                          if (resp.statusCode == 200) {
+                            final Map<String, dynamic> respMap =
+                                jsonDecode(resp.body);
+                            setState(() {
+                              _weatherText = respMap["weather"][0]["main"];
+                              _iconId = respMap["weather"][0]["icon"];
+                            });
+                          }
+                        },
+                        initialSelection: _cityIndex,
+                      )),
+                  Expanded(flex: 1, child: Text(_weatherText)),
+                  Expanded(
+                      flex: 1,
+                      child: _iconId == null
+                          ? Text("?")
+                          : Image.network(
+                              "https://openweathermap.org/img/wn/$_iconId@4x.png")),
                 ])),
             Expanded(flex: 4, child: Placeholder()),
-            Expanded(
-                flex: 2,
-                child: ElevatedButton(
-                    child: Text("change the city"),
-                    onPressed: () async {
-                      setState(() {
-                        _cityIndex = (_cityIndex + 1) % _cities.length;
-                      });
-                      final resp = await http.get(Uri.parse(
-                          "https://api.openweathermap.org/data/2.5/weather?q=${_cities[_cityIndex].$2}&appid=$_apiKeyWeather"));
-                      print(resp.body);
-                    })),
+            Expanded(flex: 2, child: Placeholder()),
           ],
         ),
       ),
